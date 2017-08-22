9a79558431e0d80ad9ee920c132ae107907adcc7
Bo Yan <boyan.au@gmail.com>
2017-08-22 16:16:09 +1000

    add new project

---
+++ coreweb0822/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace coreweb0822.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
